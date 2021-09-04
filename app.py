3ea52446b7383e7187a9bcfc3028c0c21f8393d7
JATIN KUMAWAT <56463083+jatinkumawat01@users.noreply.github.com>
2021-09-04 11:13:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,67 @@
 from flask import Flask, render_template, request, redirect, session, flash
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from mysql.connector import connect
 import random
 import string
 from flask_mail import Mail, Message
+import re
 
 app = Flask(__name__)
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=465,
     MAIL_USE_SSL=True,
-    MAIL_USERNAME='kapil8875garg@gmail.com',
-    MAIL_PASSWORD='9783874332'
+    MAIL_USERNAME='kaps4332@gmail.com',
+    MAIL_PASSWORD='Kapil8875'
 )
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kartik14@localhost/kartik'
 app.secret_key = 'ghjhjhq/213763fbf'
-
+db = SQLAlchemy(app)
 mail = Mail(app)
+urlinfo = db.Table('urlinfo', db.metadata, autoload=True, autoload_with=db.engine)
+userDetails = db.Table('userDetails', db.metadata, autoload=True, autoload_with=db.engine)
 
 
 @app.route('/')
 def shortcut():
-    # connection = connect(host='localhost', database='kartik', user='root', password='kartik14')
-    # cur = connection.cursor()
     return render_template('index.html')
 
 
 @app.route("/urlshortner")
 def urlshortner():
     url = request.args.get('link')
-    print(url)
     if url == '':
         return render_template('index.html', error2="Please enter valid url")
     custom_url = request.args.get('link1')
-    connection = connect(host="localhost", database="kartik", user="root", password="kartik14")
-    cur = connection.cursor()
     if custom_url == '':
         while True:
             encrypted_url = createEncrypted_url()
-            url_query = "select * from urlinfo where encryptedUrl='{}'".format(encrypted_url)
-            cur.execute(url_query)
-            result = cur.fetchone()
-            print(result)
-            if result is None:
+            url_query = db.session.query(urlinfo).filter_by(encryptedurl=encrypted_url).one_or_none()
+            print(url_query)
+            if url_query is None:
                 break
-        print(encrypted_url)
         if 'userid' in session:
             id = session['userid']
-            insert_query = "insert into urlinfo(originalUrl,encryptedUrl,is_Active, created_by) values('{}','{}',1,'{}')".format(
-                url, encrypted_url, id)
-        else:
-            insert_query = "insert into urlinfo(originalUrl,encryptedUrl,is_Active) values('{}','{}',1)".format(url,
-                                                                                                         encrypted_url)
-        cur = connection.cursor()
-        cur.execute(insert_query)
-        connection.commit()
+            insert_query = urlinfo.insert().values(originalUrl=url, encryptedurl=encrypted_url, is_Active=1,
+                                                   created_by=id)
+
+        else:                                                                                              encrypted_url)
+            insert_query = urlinfo.insert().values(originalUrl=url, encryptedurl=encrypted_url, is_Active=1)
+
+        db.session.execute(insert_query)
+        db.session.commit()
         final_encrypted_url = 'srt.ct/' + encrypted_url
     else:
-        url_query = "select * from urlinfo where encryptedUrl='{}'".format(custom_url)
-        cur.execute(url_query)
-        result = cur.fetchone()
-        if result is None:
+        url_query = db.session.query(urlinfo).filter_by(encryptedurl=custom_url).one_or_none()
+        if url_query is None:
             if 'userid' in session:
                 id = session['userid']
-                insert_query = "insert into urlinfo(originalUrl,encryptedUrl,is_Active, created_by) values('{}','{}',1,'{}')".format(
-                    url, custom_url, id)
+                insert_query = urlinfo.insert().values(originalUrl=url, encryptedurl=custom_url, is_Active=1,
+                                                       created_by=id)
             else:
-                insert_query = "insert into urlinfo(originalUrl,encryptedUrl,is_Active) values('{}','{}',1)".format(url,
-                                                                                                              custom_url)
-            cur.execute(insert_query)
-            connection.commit()
+                insert_query = urlinfo.insert().values(originalUrl=url, encryptedurl=custom_url, is_Active=1)
+            db.session.execute(insert_query)
+            db.session.commit()
             final_encrypted_url = 'srt.ct/' + custom_url
         else:
             if 'userid' in session:
@@ -84,27 +76,20 @@ def urlshortner():
 
 
 def createEncrypted_url():
-    # letter='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     letter = string.ascii_letters + string.digits
     encrypted_url = ''
     for i in range(6):
         encrypted_url = encrypted_url + ''.join(random.choice(letter))
-    print(encrypted_url)
     return encrypted_url
 
 
 @app.route('/<url>')
 def dynamicUrl(url):
-    connection = connect(host='localhost', database='kartik', user='root', password='kartik14')
-    url_query = "select * from urlinfo where encryptedUrl='{}'".format(url)
-    cur = connection.cursor()
-    cur.execute(url_query)
-    original_url = cur.fetchone()
-    print(original_url)
-    if original_url is None:
+    url_query = db.session.query(urlinfo).filter_by(encryptedurl=url).one_or_none()
+    if url_query[1] is None:
         return render_template('index.html')
-    print(original_url[1])
-    return redirect(original_url[1])
+    print(url_query[1])
+    return redirect(url_query[1])
 
 
 @app.route('/SignUp')
@@ -122,18 +107,21 @@ def register():
     pwd = request.args.get('pwd')
     if email == None or username == None or pwd == None:
         return render_template('SignUp.html', error1="Please fill all details")
-    connection = connect(host="localhost", database="kartik", user="root", password="kartik14")
-    cur = connection.cursor()
-    result_mail = "select * from userDetails where emailId='{}'".format(email)
-    cur.execute(result_mail)
-    result = cur.fetchone()
-    if result is None:
-        insert_query = "insert into userDetails(emailId,userName,password,is_Active) values('{}','{}','{}',1)".format(email,
-                                                                                                               username,
-                                                                                                               pwd)
-        cur = connection.cursor()
-        cur.execute(insert_query)
-        connection.commit()
+    email_check = re.fullmatch("^([a-z\d\._-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$", email)
+    if email_check is None:
+        return render_template('SignUp.html',
+                               error1="Please enter valid mail id or it should be in lowercase letters instead of uppercase")
+    psw_check = re.fullmatch("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", pwd)
+    if psw_check is None:
+        return render_template("SignUp.html", error1="Password should be of required format")
+    username_check = re.fullmatch("[a-zA-Z_\s]{5,20}", username)
+    if username_check is None:
+        return render_template("Signup.html", error1="Name should be between 5 to 20 letters without any digits and special symbol except (_)")
+    result_mail = db.session.query(userDetails).filter_by(emailId=email).one_or_none()
+    if result_mail is None:
+        insert_query = userDetails.insert().values(emailId=email, userName=username, password=pwd, is_Active=1)
+        db.session.execute(insert_query)
+        db.session.commit()
         return redirect('/Login')
     else:
         return render_template('SignUp.html', error="MailId already exist")
@@ -153,41 +141,27 @@ def checkLogIn():
     password = request.args.get('pwd')
     if email == None or password == None:
         return render_template('login.html', error="Please Fill all Details")
-    connection = connect(host="localhost", database="kartik", user="root", password="kartik14")
-    cur = connection.cursor()
-    result_mail = "select * from userDetails where emailId='{}'".format(email)
-    cur.execute(result_mail)
-    result = cur.fetchone()
-    print(result)
-    if result is None:
+    result_mail = db.session.query(userDetails).filter_by(emailId=email).one_or_none()
+    if result_mail is None:
         return render_template('login.html', error='you are not registered')
     else:
-        if password == result[3]:
+        if password == result_mail[3]:
             session['email'] = email
-            session['userid'] = result[0]
-            # return render_template('UserHome.html')
+            session['userid'] = result_mail[0]
             return redirect('/home')
         else:
             return render_template('Login.html', error='your password is not correct')
 
 
-# @app.route('/google')
-# def google():
-#     return render_template('google.html')
-
-
 @app.route('/home')
 def home():
     if 'userid' in session:
-        # email = session['email']
         id = session['userid']
         print(id)
         connection = connect(host="localhost", database="kartik", user="root", password="kartik14")
         cur = connection.cursor()
-        creator_query = "select * from urlinfo where created_by={}".format(id)
-        cur.execute(creator_query)
-        data = cur.fetchall()
-        return render_template('updateUrl.html', data=data)
+        creator_query = db.session.query(urlinfo).filter_by(created_by=id).all()
+        return render_template('updateUrl.html', data=creator_query)
     return render_template('login.html')
 
 
@@ -218,14 +192,15 @@ def updateUrl():
         encrypted = request.form.get('encrypted')
         connection = connect(host="localhost", database="kartik", user="root", password="kartik14")
         cur = connection.cursor()
-        url_query = "select * from urlinfo where encryptedUrl='{}'and pk_urlId!={}".format(encrypted, id)
-        cur.execute(url_query)
-        data = cur.fetchone()
-        if data is None:
-            update_query = "update urlinfo set originalUrl='{}', encryptedUrl='{}' where pk_urlId={}".format(url, encrypted,
-                                                                                                       id)
-            cur.execute(update_query)
-            connection.commit()
+        url_query = db.session.query(urlinfo).filter(urlinfo.c.encryptedurl == encrypted,
+                                                     urlinfo.c.pk_urlId != id).one_or_none()
+
+        print(url_query)
+        if url_query is None:
+            update_query = urlinfo.update().values(originalUrl=url, encryptedurl=encrypted).where(
+                urlinfo.c.pk_urlId == id)
+            db.session.execute(update_query)
+            db.session.commit()
             return redirect('/home')
         else:
             return render_template("editUrl.html", url=url, encrypted=encrypted, id=id, error='short url already exist')
@@ -236,11 +211,8 @@ def updateUrl():
 def deleteUrl():
     if 'userid' in session:
         id = request.form.get('id')
-        connection = connect(host="localhost", database="kartik", user="root", password="kartik14")
-        cur = connection.cursor()
-        delete_query = "delete from urlinfo where pk_urlId=" + id
-        cur.execute(delete_query)
-        connection.commit()
+        db.session.query(urlinfo).filter_by(pk_urlId=id).delete()
+        db.session.commit()
         return redirect('/home')
     return render_template('login.html')
 
@@ -253,26 +225,22 @@ def askmail():
 @app.route('/forgetpassword')
 def forgetpassword():
     email = request.args.get('email')
-    connection = connect(host="localhost", database="kartik", user="root", password="kartik14")
-    cur = connection.cursor()
-    pswd_query = "select password from userDetails where emailId='{}'".format(email)
-    cur.execute(pswd_query)
-    data = cur.fetchone()
-    print(data)
-    if data is None:
+    pswd_query = db.session.query(userDetails.c.password).filter_by(emailId=email).one_or_none()
+    if pswd_query is None:
         return render_template('askemail.html', error="email is not correct")
     randomnumber = ''
     letter = string.digits
     for i in range(6):
         randomnumber = randomnumber + ''.join(random.choice(letter))
     body = 'Your forget password OTP is ' + randomnumber
-    msg = Message(subject='Forget Password Email', sender='kapil8875garg@gmail.com',
+    msg = Message(subject='Forget Password Email', sender='kaps4332@gmail.com',
                   recipients=[email], body=body)
     msg.cc = ['kartik12@gmail.com']
     mail.send(msg)
-    update_query = "update userDetails set otp ='{}' where emailId= '{}'".format(randomnumber, email)
-    cur.execute(update_query)
-    connection.commit()
+    update_query = urlinfo.update().values(otp=randomnumber).where(
+        urlinfo.c.emailId == email)
+    db.session.execute(update_query)
+    db.session.commit()
     return render_template('updatepassword.html', email=email)
 
 
@@ -281,36 +249,18 @@ def updatepassword():
     email = request.args.get('email')
     otp = request.args.get("otp")
     password = request.args.get("new-psw")
-    connection = connect(host="localhost", database="kartik", user="root", password="kartik14")
-    cur = connection.cursor()
-    mail_query = "select otp from userDetails where emailId= '{}'".format(email)
-    pswd_query = "select password from userDetails where emailId= '{}'".format(email)
-    cur.execute(mail_query)
-    data = cur.fetchone()
-    cur.execute(pswd_query)
-    data1 = cur.fetchone()
-    if data[0] == otp:
-        if password != data1[0]:
-            update_query = "update userDetails set password ='{}' where emailId= '{}'".format(password, email)
-            cur.execute(update_query)
-            connection.commit()
+    mail_query = db.session.query(userDetails.c.otp).filter_by(emailId=email).one_or_none()
+    pswd_query = db.session.query(userDetails.c.password).filter_by(emailId=email).one_or_none()
+    if mail_query[0] == otp:
+        if password != pswd_query[0]:
+            update_query = urlinfo.update().values(password=password).where(
+                urlinfo.c.emailId == email)
+            db.session.execute(update_query)
+            db.session.commit()
             return render_template("login.html")
         else:
             return render_template("updatepassword.html", email=email, error="old password! please enter new password")
     else:
         return render_template("updatepassword.html", email=email, error="enter correct otp send to your mailid")
-
-
-# def mailbhejo():
-#     email = session['email']
-#     msg = Message(subject='mail sender', sender='kartikpanchal1409@gmail.com',
-#                   recipients=[email], body="This is my first email through python")
-#     msg.cc = []
-#     msg.html = render_template('index.html')
-#     with app.open_resource("K:/UI/bitly/images/girl_img.JPG") as f:
-#         msg.attach("1.png", "image/png", f.read())
-#     mail.send(msg)
-#     return "mail sent!!"
-
 
 app.run()
